071d7f985cb919aff60bc7271b13a3121967950f
Vic <danteckj@gmail.com>
2022-05-05 21:05:57 +0300

    Ноый шаблон

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram import executor
 
-bot = Bot(token="5149913065:AAHTUnjXbACBWNeSpcW5CSuT9nmnbeTq0PE")
-dp = Dispatcher(bot)
-
-
-@dp.message_handler()
-async def get_message(message: types.Message):
+from loader import dp
+import middlewares, filters, handlers
+from utils.notify_admins import on_startup_notify
+from utils.set_bot_commands import set_default_commands
 
 
-# sent_message =  await bot.send_message(chat_id=message.chat.id , text= "Вы сказали: " + message.text ,)
-# print(sent_message)
-# sent_photo = await bot.send_photo(message.chat.id,
-#                                   photo="https://i03.fotocdn.net/s123/76511e5d20d6fc33/public_pin_l/2813715027.jpg")
-# print(sent_photo.photo[-1].file_unique_id)
+async def on_startup(dispatcher):
+    # Устанавливаем дефолтные команды
+    await set_default_commands(dispatcher)
+
+    # Уведомляет про запуск
+    await on_startup_notify(dispatcher)
 
 
-executor.start_polling(dp)
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup)
+
